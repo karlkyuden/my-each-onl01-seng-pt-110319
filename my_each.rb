299da573c64d5a4f3b20ead80d71124c049d3062
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-11-11 18:19:23 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,15 @@
 def my_each (array)
   # code here
   index = 0
-  while index < array.length do 
-    yield(array[index])
-    index += 1
+  if block_given?
+    i = 0
+    while i < array.length
+      yield(array[index])
+      i+= 1
+    end
+    array
+  else
+    puts "No block was given"
   end
-  array
+  
 end
