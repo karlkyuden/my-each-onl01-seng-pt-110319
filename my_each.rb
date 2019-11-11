fb28e12df7f3713d29cb4f5e416a7e701105d8c5
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-11-11 18:11:13 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,9 @@
 def my_each (array)
   # code here
   index = 0
-  newArray = []
   while index < array.length do 
-    newArray << array[index]
-    print array[index]
+    print "array[index]\n"
     index += 1
   end
-  newArray
+  
 end
